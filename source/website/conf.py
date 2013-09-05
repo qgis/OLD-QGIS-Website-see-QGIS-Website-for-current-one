0187278d75b3393ba427b447a0c1e0eaf64f9910
Richard Duivenvoorde <richard@duif.net>
2013-09-05 08:46:43 +0200

    removing autodoc extension as it appears not needed

--- source/website/conf.py
+++ source/website/conf.py
@@ -26,7 +26,7 @@ import sphinx_bootstrap_theme
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['../../themes/qgis-theme']
