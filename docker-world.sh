a1f8ded1141be0625ffe14ce46ad97e033b72041
Richard Duivenvoorde <richard@duif.net>
2014-05-25 11:05:27 +0200

    make it possible to run just one language build from cli

--- docker-world.sh
+++ docker-world.sh
@@ -1,7 +1,13 @@
 
 # only languages which have translations in transifex
-for LANG in en ca de es fa fi fr id it ja km_KH ko nl pl pt_BR pt_PT ro tr ru uk zh_CN
+: ${langs:=en ca de es fa fi fr id it ja km_KH ko nl pl pt_BR pt_PT ro tr ru uk zh_CN}
+
+# if you only want to build one language, do:
+# $ langs=de ./docker-world.sh
+
+#for LANG in en ca de es fa fi fr id it ja km_KH ko nl pl pt_BR pt_PT ro tr ru uk zh_CN
+for l in $langs
   do
-    /bin/bash ./docker-run.sh full LANG=$LANG
-    rsync -hvrz -e ssh --progress output/html/$LANG qgis.osgeo.osuosl.org:/var/www/qgis_new_web2/QGIS-Website/live/html
+    /bin/bash ./docker-run.sh full LANG=$l
+    rsync -hvrz -e ssh --progress output/html/$l qgis.osgeo.osuosl.org:/var/www/qgis_new_web/QGIS-Website/live/html
   done
