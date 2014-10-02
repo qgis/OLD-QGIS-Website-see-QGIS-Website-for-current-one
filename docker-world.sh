cf77132b622852cbb5e69283e992c98422fd73a7
Richard Duivenvoorde <richard@duif.net>
2014-10-02 12:44:04 +0200

    adding git lines to make it work in cron

--- docker-world.sh
+++ docker-world.sh
@@ -1,11 +1,20 @@
 
+# cd to script dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+# throw away building artefacts
+git stash
+git stash drop
+# get latest version
+git pull
+
 # only languages which have translations in transifex
-: ${langs:=en ca de es fa fi fr id it ja km_KH ko nl pl pt_BR pt_PT ro tr ru uk zh_CN}
+#: ${langs:=en ca de es fa fi fr id it ja km_KH ko nl pl pt_BR pt_PT ro tr ru uk zh_CN}
+: ${langs:=en}
 
 # if you only want to build one language, do:
 # $ langs=de ./docker-world.sh
 
-#for LANG in en ca de es fa fi fr id it ja km_KH ko nl pl pt_BR pt_PT ro tr ru uk zh_CN
 for l in $langs
   do
     /bin/bash ./docker-run.sh full LANG=$l
