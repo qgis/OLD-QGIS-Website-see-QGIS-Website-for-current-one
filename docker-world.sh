59ab5b9a13422db47fabc836729f87224bc34f03
Richard Duivenvoorde <richard@duif.net>
2014-10-10 19:22:07 +0200

    start building all languages again

--- docker-world.sh
+++ docker-world.sh
@@ -9,8 +9,7 @@ git stash drop
 git pull
 
 # only languages which have translations in transifex
-#: ${langs:=en ca de es fa fi fr id it ja km_KH ko nl pl pt_BR pt_PT ro tr ru uk zh_CN}
-: ${langs:=en}
+: ${langs:=en ca de es fa fi fr id it ja km_KH ko nl pl pt_BR pt_PT ro tr ru uk zh_CN}
 
 # if you only want to build one language, do:
 # $ langs=de ./docker-world.sh
