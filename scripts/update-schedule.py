1ec4ccdad03a17aabe216d0397686c003d6c9216
Richard Duivenvoorde <richard@duif.net>
2017-07-18 11:57:22 +0200

    python3ize script

--- scripts/update-schedule.py
+++ scripts/update-schedule.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import urllib2
+from urllib.request import urlopen
 import csv
+import codecs
 
 url = "https://docs.google.com/spreadsheets/u/1/d/1MOIjwon5eDI04DG6rX_HwucZkW1fxFJ0b_yB0xYETOE/export?format=csv&id=1MOIjwon5eDI04DG6rX_HwucZkW1fxFJ0b_yB0xYETOE&gid=1982100417#"
 
@@ -15,8 +16,8 @@ Event      Latest  Long-Term  FF   Date       Week Weeks
 ========== ======= ========= ===== ========== ==== =====
 """)
 
-csvfile = urllib2.urlopen(url)
-reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+resource = urlopen(url)
+reader = csv.reader(codecs.iterdecode(resource, 'utf-8'), delimiter=',', quotechar='"')
 first = True
 for row in reader:
     if first:
@@ -29,8 +30,6 @@ for row in reader:
     event = event.replace('LTR', '**LTR**')
 
     o.write("{0:10s} {1:7s} {2:9s} {3:5s} {4:10s} {5:5s} {6}\n".format(event, lr, ltr, dev, date, weekno, weeks))
-
-csvfile.close()
 
 o.write("""\
 ========== ======= ========= ===== ========== ==== =====
