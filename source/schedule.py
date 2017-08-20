4ae17c11feec77b581a6b95a11f0d5a16ffc59ab
Juergen E. Fischer <jef@norbit.de>
2017-08-20 12:25:55 +0200

    Update for 2.18.12/2.14.18 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -2,15 +2,15 @@ from datetime import date
 
 # latest release
 version = '2.18'
-release = '2.18.11'
+release = '2.18.12'
 codename = u'Las Palmas'
 binary = '1'
-releasedate = date(2017, 7, 21)
+releasedate = date(2017, 8, 18)
 releasenote = '(new LTR)'
 
 # long term release repository
 ltrversion = '2.14'
-ltrrelease = '2.14.17'
+ltrrelease = '2.14.18'
 ltrcodename = u'Essen'
 ltrbinary = '1'
 ltrnote = '(previous LTR)'
@@ -19,4 +19,4 @@ devversion = '2.99'
 nextversion = '3.0'
 nextfreezedate = '2017-09-15 12:00:00 UTC'
 nextreleasedate = '2017-11-24 12:00:00 UTC'
-nextpointreleasedate = '2017-08-18 12:00:00 UTC'
+nextpointreleasedate = '2017-09-15 12:00:00 UTC'
