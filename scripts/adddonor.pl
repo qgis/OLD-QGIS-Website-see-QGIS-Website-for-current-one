cf195bf16de076e7298e0e870c413f62fd489c31
Juergen E. Fischer <jef@norbit.de>
2018-01-24 20:56:12 +0100

    Remove reversed donation (reverts 09c414d03)

---
+++ scripts/adddonor.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my %donors;
+
+open I, "source/site/about/donors.inc";
+while(<I>) {
+        chop;
+        next unless /^\s*[*-]/;
+        s/^[-*  ]+//;
+        next if /^\s*$/;
+        $donors{lc $_}=$_;
+}
+close I;
+
+while(<>) {
+	chop;
+	$donors{lc $_} = $_;
+}
+
+open O, ">source/site/about/donors.inc";
+print O ".. list-table:: List of Donors\n\n";
+
+my $cols = 3;
+
+my $i=0;
+foreach ( sort { lc($a) cmp lc($b) } keys %donors ) {
+        print O "   " . ($i%$cols==0 ? "*" : " ") . " - $donors{$_}\n";
+        $i++;
+}
+print O "     -\n" while $i++%$cols>0;
