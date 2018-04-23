9ee92f7ee7e0a3268a45a6e9dc9fadb00c576315
William Kyngesburye <kyngchaos@sumomo.attlocal.net>
2018-04-23 08:28:35 -0500

    separate mac version numbers for downloads

--- source/schedule.py
+++ source/schedule.py
@@ -3,16 +3,20 @@ from datetime import date
 # latest release
 version = '3.0'
 release = '3.0.2'
+release_mac = '3.0.1'
 codename = u'Girona'
 binary = '1'
+binary_mac = '1'
 releasedate = date(2018, 4, 20)
 releasenote = '​'
 
 # long term release repository
 ltrversion = '2.18'
 ltrrelease = '2.18.19'
+ltrrelease_mac = '2.18.18'
 ltrcodename = u'Las Palmas'
 ltrbinary = '1'
+ltrbinary_mac = '1'
 ltrnote = 'LTR'
 
 devversion = '3.1'
