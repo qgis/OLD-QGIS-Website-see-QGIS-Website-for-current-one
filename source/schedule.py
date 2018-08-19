b20ef42ce2ea4a457643c4f857d454f6c98f6efc
Juergen E. Fischer <jef@norbit.de>
2018-08-19 18:25:09 +0200

    Update for 3.2.2/2.18.23 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -2,15 +2,15 @@ from datetime import date
 
 # latest release
 version = '3.2'
-release = '3.2.1'
+release = '3.2.2'
 codename = u'Bonn'
 binary = '1'
-releasedate = date(2018, 7, 20)
+releasedate = date(2018, 8, 17)
 releasenote = '​'
 
 # long term release repository
 ltrversion = '2.18'
-ltrrelease = '2.18.22'
+ltrrelease = '2.18.23'
 ltrcodename = u'Las Palmas'
 ltrbinary = '1'
 ltrnote = 'LTR'
@@ -19,6 +19,6 @@ devversion = '3.3'
 nextversion = '3.4'
 nextfreezedate = '2018-09-14 12:00:00 UTC'
 nextreleasedate = '2018-10-26 12:00:00 UTC'
-nextpointreleasedate = '2018-08-17 12:00:00 UTC'
+nextpointreleasedate = '2018-09-14 12:00:00 UTC'
 
 infeaturefreeze = False
