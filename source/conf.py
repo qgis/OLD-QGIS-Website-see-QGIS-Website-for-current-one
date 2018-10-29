19c6e6105b36f8f6d65782439b3eecb6bfa8f591
William Kyngesburye <kyngchaos@sumomo.attlocal.net>
2018-10-29 18:57:46 -0500

    mac version update

--- source/conf.py
+++ source/conf.py
@@ -337,9 +337,9 @@ context = {
     'binary': binary,
     'ltrbinary': ltrbinary,
     'infeaturefreeze': infeaturefreeze,
-    'release_mac': '3.2.3',
+    'release_mac': '3.4.0',
     'binary_mac': '1',
-    'ltrrelease_mac': '2.18.24',
+    'ltrrelease_mac': '2.18.25',
     'ltrbinary_mac': '1',
     'stripeformurl': '/stripe/form'
 }
