4d457d0ad217ec3e45e1aa24277c5ed84bfdffc3
Juergen E. Fischer <jef@norbit.de>
2018-11-02 14:00:27 +0100

    Update for 3.4.1 extra point release

--- source/schedule.py
+++ source/schedule.py
@@ -2,10 +2,10 @@ from datetime import date
 
 # latest release
 version = '3.4'
-release = '3.4.0'
+release = '3.4.1'
 codename = u'Madeira'
 binary = '1'
-releasedate = date(2018, 10, 26)
+releasedate = date(2018, 11, 2)
 releasenote = '(new LTR)'
 
 # long term release repository
@@ -19,6 +19,6 @@ devversion = '3.5'
 nextversion = '3.6'
 nextfreezedate = '2019-01-18 12:00:00 UTC'
 nextreleasedate = '2019-02-22 12:00:00 UTC'
-nextpointreleasedate = '2018-11-02 12:00:00 UTC'
+nextpointreleasedate = '2018-11-23 12:00:00 UTC'
 
 infeaturefreeze = False
