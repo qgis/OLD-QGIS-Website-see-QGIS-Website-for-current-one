a2bfbb884a77d2b2b5903b43dff28dff56db3724
Juergen E. Fischer <jef@norbit.de>
2019-01-18 10:46:06 +0100

    change version.txt link to https

--- scripts/mkversion.py
+++ scripts/mkversion.py
@@ -4,7 +4,7 @@ exec(open("source/schedule.py").read())
 
 f = open("output/html/version.txt", "w")
 
-f.write( """#QGIS Version %(versionint)d|Visit http://download.qgis.org to get your copy of version %(version)s|<html><body>
+f.write( """#QGIS Version %(versionint)d|Visit https://download.qgis.org to get your copy of version %(version)s|<html><body>
 <ul>
 <li>The current released version of QGIS is %(version)s.
 <li>We are working on %(devversion)s-Master. It will go into feature freeze on %(freezedate)s and be released as %(nextversion)s on %(nextreleasedate)s.
