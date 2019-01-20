12dd8a199945dd7e7b0a9cbe439f07b134be9d62
Juergen E. Fischer <jef@norbit.de>
2019-01-20 18:11:37 +0100

    Update for 3.4.4/2.18.28 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -2,23 +2,23 @@ from datetime import date
 
 # latest release
 version = '3.4'
-release = '3.4.3'
+release = '3.4.4'
 codename = u'Madeira'
 binary = '1'
-releasedate = date(2018, 12, 21)
+releasedate = date(2019, 1, 18)
 releasenote = '(new LTR)'
 
 # long term release repository
 ltrversion = '2.18'
-ltrrelease = '2.18.27'
+ltrrelease = '2.18.28'
 ltrcodename = u'Las Palmas'
 ltrbinary = '1'
-ltrnote = '(old LTR)'
+ltrnote = '(old LTR; EOL)'
 
 devversion = '3.5'
 nextversion = '3.6'
 nextfreezedate = '2019-01-18 12:00:00 UTC'
 nextreleasedate = '2019-02-22 12:00:00 UTC'
-nextpointreleasedate = '2019-01-18 12:00:00 UTC'
+nextpointreleasedate = '2019-02-22 12:00:00 UTC'
 
-infeaturefreeze = False
+infeaturefreeze = True
