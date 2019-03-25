461ec259a9610b2115878a2097a574c148e2998e
Juergen E. Fischer <jef@norbit.de>
2019-03-25 22:40:10 +0100

    Update for 3.6.1/3.4.6 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -2,23 +2,23 @@ from datetime import date
 
 # latest release
 version = '3.6'
-release = '3.6.0'
+release = '3.6.1'
 codename = u'Noosa'
 binary = '1'
-releasedate = date(2019, 2, 22)
+releasedate = date(2019, 3, 22)
 releasenote = u'\u200B'
 
 # long term release repository
 ltrversion = '3.4'
-ltrrelease = '3.4.5'
+ltrrelease = '3.4.6'
 ltrcodename = u'Madeira'
-ltrbinary = '1'
+ltrbinary = '2'
 ltrnote = u'LTR'
 
 devversion = '3.7'
 nextversion = '3.6'
 nextfreezedate = '2019-04-26 12:00:00 UTC'
 nextreleasedate = '2019-06-21 12:00:00 UTC'
-nextpointreleasedate = '2019-03-22 12:00:00 UTC'
+nextpointreleasedate = '2019-04-19 12:00:00 UTC'
 
 infeaturefreeze = False
