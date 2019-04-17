e12d6f39b9c02c7936d303fccfc37df85e0237af
William Kyngesburye <kyngchaos@Sumomo.attlocal.net>
2019-04-16 19:23:48 -0500

    mac binary bump

--- source/conf.py
+++ source/conf.py
@@ -338,9 +338,9 @@ context = {
     'ltrbinary': ltrbinary,
     'infeaturefreeze': infeaturefreeze,
     'release_mac': '3.6.1',
-    'binary_mac': '2',
+    'binary_mac': '3',
     'ltrrelease_mac': '3.4.6',
-    'ltrbinary_mac': '2',
+    'ltrbinary_mac': '3',
     'stripeformurl': '/stripe/form'
 }
 
