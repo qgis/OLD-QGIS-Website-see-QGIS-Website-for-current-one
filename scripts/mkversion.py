0fcdd53be8825111a1e98b8450b13d588467b625
Juergen E. Fischer <jef@norbit.de>
2019-07-31 23:48:15 +0200

    update progress link in version.txt to github

--- scripts/mkversion.py
+++ scripts/mkversion.py
@@ -8,7 +8,7 @@ f.write( """#QGIS Version %(versionint)d|Visit https://download.qgis.org to get 
 <ul>
 <li>The current released version of QGIS is %(version)s.
 <li>We are working on %(devversion)s-Master. It will go into feature freeze on %(freezedate)s and be released as %(nextversion)s on %(nextreleasedate)s.
-<li>Details on changes can be found by examining the Activity in Redmine at <a href="https://issues.qgis.org/projects/qgis">https://issues.qgis.org/projects/qgis</a>.
+<li>Details on changes can be found on github at <a href="https://github.com/qgis/QGIS/commits/master">https://github.com/qgis/QGIS/commits/master</a>.
 </body>
 </html>
 """ % {
