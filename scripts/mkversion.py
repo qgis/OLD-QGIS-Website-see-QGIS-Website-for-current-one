e10533371f6fc163b69b0f59c0add2d191601e48
Juergen E. Fischer <jef@norbit.de>
2019-08-24 17:01:31 +0200

    add version-ltr.txt which requests coming from QGIS LTR requests are rewritten to (refs qgis/QGIS#31298)

--- scripts/mkversion.py
+++ scripts/mkversion.py
@@ -21,3 +21,25 @@ f.write( """#QGIS Version %(versionint)d|Visit https://download.qgis.org to get 
 } )
 
 f.close()
+
+f = open("output/html/version-ltr.txt", "w")
+
+f.write( """#QGIS Version %(versionint)d|Visit https://download.qgis.org to get your copy of version %(ltrversion)s|<html><body>
+<ul>
+<li>The current available LTR version of QGIS is %(ltrversion)s.
+<li>The latest available version of QGIS is %(version)s.
+<li>We are working on %(devversion)s-Master. It will go into feature freeze on %(freezedate)s and be released as %(nextversion)s on %(nextreleasedate)s.
+<li>Details on changes can be found on github at <a href="https://github.com/qgis/QGIS/commits/master">https://github.com/qgis/QGIS/commits/master</a>.
+</body>
+</html>
+""" % {
+	"versionint": int("%d%02d%02d" % tuple( map(int, ltrrelease.split(".")))),
+	"ltrversion": ltrrelease,
+        "version": release,
+	"devversion": devversion,
+	"freezedate": nextfreezedate if nextfreezedate is not None else "a not yet determined date",
+	"nextreleasedate": nextreleasedate if nextreleasedate is not None else "a not yet determined date",
+	"nextversion": nextversion,
+} )
+
+f.close()
