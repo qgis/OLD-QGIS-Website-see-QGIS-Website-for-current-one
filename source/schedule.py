d0d0073a15a7fa117ec0ee948df20723b15360d9
Juergen E. Fischer <jef@norbit.de>
2019-10-28 11:07:36 +0100

    re-run make schedule after forgotten spreadsheet update (superceeds #694)

--- source/schedule.py
+++ source/schedule.py
@@ -3,7 +3,7 @@ from datetime import date
 # latest release
 version = '3.10'
 release = '3.10.0'
-codename = u'Zanzibar'
+codename = u'A Coruña'
 binary = '1'
 releasedate = date(2019, 10, 25)
 releasenote = u'\u200B'
@@ -15,7 +15,7 @@ ltrcodename = u'Madeira'
 ltrbinary = '1'
 ltrnote = u'LTR'
 
-devversion = '3.9'
+devversion = '3.11'
 nextversion = '3.10'
 nextfreezedate = '2020-01-17 12:00:00 UTC'
 nextreleasedate = '2020-02-21 12:00:00 UTC'
