4d6c3f37ba95f22e5b87400a2db9a9546b535ed1
Juergen E. Fischer <jef@norbit.de>
2019-11-22 08:40:31 +0100

    Delay 3.10.1/3.4.14 point releases for a week
    https://lists.osgeo.org/pipermail/qgis-developer/2019-November/059354.html

--- source/schedule.py
+++ source/schedule.py
@@ -19,6 +19,6 @@ devversion = '3.11'
 nextversion = '3.12'
 nextfreezedate = '2020-01-17 12:00:00 UTC'
 nextreleasedate = '2020-02-21 12:00:00 UTC'
-nextpointreleasedate = '2019-11-22 12:00:00 UTC'
+nextpointreleasedate = '2019-11-29 12:00:00 UTC'
 
 infeaturefreeze = False
