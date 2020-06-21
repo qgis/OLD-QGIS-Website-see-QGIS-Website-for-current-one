41555f8feb68b063741be9f245f5e2644d28ad67
Juergen E. Fischer <jef@norbit.de>
2020-06-21 18:10:04 +0200

    Update for 3.14.0/3.10.7 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -1,24 +1,24 @@
 from datetime import date
 
 # latest release
-version = '3.12'
-release = '3.12.3'
+version = '3.14'
+release = '3.14.0'
 codename = u'București'
 binary = '1'
-releasedate = date(2020, 5, 15)
+releasedate = date(2020, 6, 19)
 releasenote = u'\u200B'
 
 # long term release repository
 ltrversion = '3.10'
-ltrrelease = '3.10.6'
+ltrrelease = '3.10.7'
 ltrcodename = u'A Coruña'
 ltrbinary = '1'
 ltrnote = u'LTR'
 
 devversion = '3.13'
 nextversion = '3.14'
-nextfreezedate = '2020-05-15 12:00:00 UTC'
-nextreleasedate = '2020-06-19 12:00:00 UTC'
-nextpointreleasedate = '2020-06-19 12:00:00 UTC'
+nextfreezedate = '2020-09-11 12:00:00 UTC'
+nextreleasedate = '2020-10-23 12:00:00 UTC'
+nextpointreleasedate = '2020-07-17 12:00:00 UTC'
 
-infeaturefreeze = True
+infeaturefreeze = False
