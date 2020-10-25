7feb5c3d2e576047973d5f0d2ae9fef3d19ec3fd
Juergen E. Fischer <jef@norbit.de>
2020-10-25 21:37:07 +0100

    Update for 3.16.0/3.10.11 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -1,24 +1,24 @@
 from datetime import date
 
 # latest release
-version = '3.14'
-release = '3.14.16'
+version = '3.16'
+release = '3.16.0'
 codename = u'Pi'
 binary = '2'
-releasedate = date(2020, 9, 11)
+releasedate = date(2020, 10, 23)
 releasenote = u'\u200B'
 
 # long term release repository
 ltrversion = '3.10'
-ltrrelease = '3.10.10'
+ltrrelease = '3.10.11'
 ltrcodename = u'A Coruña'
 ltrbinary = '2'
 ltrnote = u'LTR'
 
 devversion = '3.15'
 nextversion = '3.16'
-nextfreezedate = '2020-09-11 12:00:00 UTC'
-nextreleasedate = '2020-10-23 12:00:00 UTC'
-nextpointreleasedate = '2020-10-23 12:00:00 UTC'
+nextfreezedate = '2021-01-15 12:00:00 UTC'
+nextreleasedate = '2021-02-19 12:00:00 UTC'
+nextpointreleasedate = '2020-11-20 12:00:00 UTC'
 
-infeaturefreeze = True
+infeaturefreeze = False
