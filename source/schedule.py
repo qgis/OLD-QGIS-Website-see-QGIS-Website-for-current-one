e5d63586d7e93e932afc1b2cc45a423b512930e1
Juergen E. Fischer <jef@norbit.de>
2020-10-26 10:14:30 +0100

    Update spreadsheet

--- source/schedule.py
+++ source/schedule.py
@@ -3,8 +3,8 @@ from datetime import date
 # latest release
 version = '3.16'
 release = '3.16.0'
-codename = u'Pi'
-binary = '2'
+codename = u'Hannover'
+binary = '1'
 releasedate = date(2020, 10, 23)
 releasenote = u'\u200B'
 
@@ -15,8 +15,8 @@ ltrcodename = u'A Coruña'
 ltrbinary = '2'
 ltrnote = u'LTR'
 
-devversion = '3.15'
-nextversion = '3.16'
+devversion = '3.17'
+nextversion = '3.18'
 nextfreezedate = '2021-01-15 12:00:00 UTC'
 nextreleasedate = '2021-02-19 12:00:00 UTC'
 nextpointreleasedate = '2020-11-20 12:00:00 UTC'
