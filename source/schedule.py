ab660b4fe9540ea6174ec8c5e7230db6ebaae13e
Juergen E. Fischer <jef@norbit.de>
2021-05-26 11:38:07 +0200

    MSIs updated

--- source/schedule.py
+++ source/schedule.py
@@ -5,7 +5,7 @@ version = '3.18'
 release = '3.18.3'
 codename = u'Zürich'
 binary = '1'
-msibinary = '1'
+msibinary = '2'
 releasedate = date(2021, 5, 14)
 releasenote = u'\u200B'
 
@@ -14,7 +14,7 @@ ltrversion = '3.16'
 ltrrelease = '3.16.7'
 ltrcodename = u'Hannover'
 ltrbinary = '1'
-msiltrbinary = '1'
+msiltrbinary = '2'
 ltrnote = u'LTR'
 
 devversion = '3.19'
