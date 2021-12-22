a01b4e4391f55b102e01479c86543ecc1a818b99
Juergen E. Fischer <jef@norbit.de>
2021-12-22 19:15:31 +0100

    Update for 3.22.2/3.16.15 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -2,16 +2,16 @@ from datetime import date
 
 # latest release
 version = '3.22'
-release = '3.22.1'
+release = '3.22.2'
 codename = u'Białowieża'
 binary = '1'
 msibinary = '1'
-releasedate = date(2021, 11, 19)
+releasedate = date(2021, 12, 17)
 releasenote = u'\u200B'
 
 # long term release repository
 ltrversion = '3.16'
-ltrrelease = '3.16.14'
+ltrrelease = '3.16.15'
 ltrcodename = u'Hannover'
 ltrbinary = '1'
 msiltrbinary = '1'
@@ -21,6 +21,6 @@ devversion = '3.23'
 nextversion = '3.24'
 nextfreezedate = '2022-01-14 12:00:00 UTC'
 nextreleasedate = '2022-02-18 12:00:00 UTC'
-nextpointreleasedate = '2021-12-17 12:00:00 UTC'
+nextpointreleasedate = '2022-01-14 12:00:00 UTC'
 
 infeaturefreeze = False
