734aad4be83f8e866b8f72512d1e9b3843d0323b
Juergen E. Fischer <jef@norbit.de>
2022-05-19 14:21:08 +0200

    Update for 3.24.3/3.22.7 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -2,15 +2,15 @@ from datetime import date
 
 # latest release
 version = '3.24'
-release = '3.24.2'
+release = '3.24.3'
 codename = u'Tisler'
 binary = '1'
-releasedate = date(2022, 4, 15)
+releasedate = date(2022, 5, 13)
 releasenote = u'\u200B'
 
 # long term release repository
 ltrversion = '3.22'
-ltrrelease = '3.22.6'
+ltrrelease = '3.22.7'
 ltrcodename = u'Białowieża'
 ltrbinary = '1'
 ltrnote = u'LTR'
@@ -19,6 +19,6 @@ devversion = '3.25'
 nextversion = '3.26'
 nextfreezedate = '2022-05-13 12:00:00 UTC'
 nextreleasedate = '2022-06-17 12:00:00 UTC'
-nextpointreleasedate = '2022-05-13 12:00:00 UTC'
+nextpointreleasedate = '2022-06-17 12:00:00 UTC'
 
-infeaturefreeze = False
+infeaturefreeze = True
