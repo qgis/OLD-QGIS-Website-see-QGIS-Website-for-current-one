311173cd6e407e09e4ebfe8867a503c92a17e118
Juergen E. Fischer <jef@norbit.de>
2022-07-06 11:24:04 +0200

    Update msi after OSGeo4W fixes

--- source/schedule.py
+++ source/schedule.py
@@ -4,7 +4,7 @@ from datetime import date
 version = '3.26'
 release = '3.26.0'
 codename = u'Buenos Aires'
-binary = '3'
+binary = '4'
 releasedate = date(2022, 6, 17)
 releasenote = u'RC'
 
@@ -12,7 +12,7 @@ releasenote = u'RC'
 ltrversion = '3.22'
 ltrrelease = '3.22.8'
 ltrcodename = u'Białowieża'
-ltrbinary = '3'
+ltrbinary = '4'
 ltrnote = u'LTR'
 
 devversion = '3.27'
