d2c4670b3fbfc7835652602f357497a058760bbc
Juergen E. Fischer <jef@norbit.de>
2022-12-18 22:34:04 +0100

    Update for 3.28.2/3.22.14 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -2,23 +2,23 @@ from datetime import date
 
 # latest release
 version = '3.28'
-release = '3.28.1'
+release = '3.28.2'
 codename = u'Firenze'
-binary = '3'
-releasedate = date(2022, 11, 18)
+binary = '1'
+releasedate = date(2022, 12, 16)
 releasenote = u'\u200B'
 
 # long term release repository
 ltrversion = '3.22'
-ltrrelease = '3.22.13'
+ltrrelease = '3.22.14'
 ltrcodename = u'Białowieża'
-ltrbinary = '3'
+ltrbinary = '1'
 ltrnote = u'LTR'
 
 devversion = '3.29'
 nextversion = '3.30'
 nextfreezedate = '2023-01-13 12:00:00 UTC'
 nextreleasedate = '2023-02-17 12:00:00 UTC'
-nextpointreleasedate = '2022-12-16 12:00:00 UTC'
+nextpointreleasedate = '2023-01-13 12:00:00 UTC'
 
 infeaturefreeze = False
