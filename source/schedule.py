4341b1e1056f18056f0c145d49c1a3dcce2639ad
Juergen E. Fischer <jef@norbit.de>
2023-01-30 12:33:36 +0100

    Update for 3.28.3/3.22.15 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -2,15 +2,15 @@ from datetime import date
 
 # latest release
 version = '3.28'
-release = '3.28.2'
+release = '3.28.3'
 codename = u'Firenze'
 binary = '1'
-releasedate = date(2022, 12, 16)
+releasedate = date(2023, 1, 27)
 releasenote = u'\u200B'
 
 # long term release repository
 ltrversion = '3.22'
-ltrrelease = '3.22.14'
+ltrrelease = '3.22.15'
 ltrcodename = u'Białowieża'
 ltrbinary = '1'
 ltrnote = u'LTR'
@@ -19,6 +19,6 @@ devversion = '3.29'
 nextversion = '3.30'
 nextfreezedate = '2023-01-27 12:00:00 UTC'
 nextreleasedate = '2023-03-03 12:00:00 UTC'
-nextpointreleasedate = '2023-01-27 12:00:00 UTC'
+nextpointreleasedate = '2023-03-03 12:00:00 UTC'
 
-infeaturefreeze = False
+infeaturefreeze = True
