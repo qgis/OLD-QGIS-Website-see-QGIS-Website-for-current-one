c30bc29ef45e33a2fd6b95f9ce85bfc3184aaaf6
Juergen E. Fischer <jef@norbit.de>
2023-06-26 22:13:05 +0200

    Update for 3.32.0/3.28.8 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -1,24 +1,24 @@
 from datetime import date
 
 # latest release
-version = '3.30'
-release = '3.30.3'
-codename = u'\'s-Hertogenbosch'
+version = '3.32'
+release = '3.32.0'
+codename = u'Lima'
 binary = '1'
-releasedate = date(2023, 5, 26)
-releasenote = u'\u200B'
+releasedate = date(2023, 6, 23)
+releasenote = u'RC'
 
 # long term release repository
 ltrversion = '3.28'
-ltrrelease = '3.28.7'
+ltrrelease = '3.28.8'
 ltrcodename = u'Firenze'
 ltrbinary = '1'
 ltrnote = u'LTR'
 
-devversion = '3.31'
-nextversion = '3.32'
-nextfreezedate = '2023-05-26 12:00:00 UTC'
-nextreleasedate = '2023-06-23 12:00:00 UTC'
-nextpointreleasedate = '2023-06-23 12:00:00 UTC'
+devversion = '3.33'
+nextversion = '3.34'
+nextfreezedate = '2023-09-15 12:00:00 UTC'
+nextreleasedate = '2023-10-27 12:00:00 UTC'
+nextpointreleasedate = '2023-07-21 12:00:00 UTC'
 
-infeaturefreeze = True
+infeaturefreeze = False
