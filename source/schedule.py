6c76a8a28a2bddb3a9f024585aad502150f41b72
Juergen E. Fischer <jef@norbit.de>
2024-03-26 15:24:18 +0100

    Update for 3.36.1/3.34.5 point releases

--- source/schedule.py
+++ source/schedule.py
@@ -2,16 +2,16 @@ from datetime import date
 
 # latest release
 version = '3.36'
-release = '3.36.0'
+release = '3.36.1'
 codename = u'Maidenhead'
 binary = '1'
-releasedate = date(2024, 2, 23)
-releasenote = u'RC'
+releasedate = date(2024, 3, 22)
+releasenote = u'\u200B'
 shortver = u'336'
 
 # long term release repository
 ltrversion = '3.34'
-ltrrelease = '3.34.4'
+ltrrelease = '3.34.5'
 ltrcodename = u'Prizren'
 ltrbinary = '1'
 ltrnote = u'LTR'
@@ -20,6 +20,6 @@ devversion = '3.37'
 nextversion = '3.38'
 nextfreezedate = '2024-05-17 12:00:00 UTC'
 nextreleasedate = '2024-06-21 12:00:00 UTC'
-nextpointreleasedate = '2024-03-22 12:00:00 UTC'
+nextpointreleasedate = '2024-04-19 12:00:00 UTC'
 
 infeaturefreeze = False
